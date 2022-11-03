5a2ffa4eab44c2ed823bcc29f8a341af427f557f
WooJoo <pine951128@hanmail.net>
2022-11-03 23:46:30 +0900

    로컬환경 다시 세팅

---
+++ Code/_14TwoDimensional/01TwoDimensional.py
@@ -0,0 +1,13 @@
+"""
+2차원 리스트 
+똑같음
+"""
+person_info = [
+    ["유재석", 51, "남"],
+    ["노홍철", 74, "남"],
+    ["하하", 44, "남"]
+]
+
+print(person_info)
+print(person_info[0][0])
+print(person_info[1][1])
